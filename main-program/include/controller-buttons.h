162200ea701bb0df541cda90f6e2e123e21898d3
VEXObsessed <49462576+vexobsessed@users.noreply.github.com>
2020-08-01 17:39:28 -0500

    Added initializer lists

--- main-program/include/controller-buttons.h
+++ main-program/include/controller-buttons.h
@@ -15,6 +15,91 @@
 #include <bits/stdc++.h>
 
 namespace controllerbuttons {
+
+// class ButtonHandler {
+//   private:
+//   class ControllerCallback {
+//     private:
+//     class ButtonCallback {
+//       private:
+//       class ButtonTrigger {
+//         private:
+//         public:
+//         pros::Controller * controller_;
+//         pros::controller_digital_e_t button_;
+
+//         std::function<void()> function_;
+//         bool was_triggered_ = false;
+//         bool is_set = false;
+//         bool trigger_on_release;
+//         ButtonTrigger(pros::Controller * controller, pros::controller_digital_e_t button, bool trigger_on_release_arg);
+        
+//         void set(std::function<void()> function);
+
+//         void clear();
+
+//         void run_if_triggered();
+//       };
+//       public:
+
+//       pros::Controller * controller_;
+//       pros::controller_digital_e_t button_;
+      
+//       ButtonCallback(pros::Controller * controller, pros::controller_digital_e_t button) {
+//         pressed = new ButtonTrigger(controller, button, false);
+//         released = new ButtonTrigger(controller, button, true);
+//       }
+
+//       ButtonTrigger *pressed;
+//       ButtonTrigger *released;
+
+//       void run();
+//     };
+
+//     pros::Controller * controller_;
+
+//     public:
+//     ControllerCallback(pros::Controller * controller);
+
+//     ButtonCallback *l1;
+//     ButtonCallback *l2;
+//     ButtonCallback *r1;
+//     ButtonCallback *r2;
+//     ButtonCallback *up;
+//     ButtonCallback *down;
+//     ButtonCallback *left;
+//     ButtonCallback *right;
+//     ButtonCallback *x;
+//     ButtonCallback *b;
+//     ButtonCallback *y;
+//     ButtonCallback *a;
+
+//     std::vector<ButtonCallback *> button_vector = {l1, l2, r1, r2, up, down, left, right, x, b, y, a};
+
+//     void run();
+//   };
+
+//   pros::Controller * master_controller_;
+//   pros::Controller * partner_controller_;
+
+//   public:
+//   ButtonHandler(pros::Controller * master_controller, pros::Controller * partner_controller);
+//   ControllerCallback *master;
+//   ControllerCallback *partner;
+  
+//   void run();
+// };
+
+// extern ButtonHandler button_handler;
+
+
+
+
+
+
+
+
+
 extern bool unnasigned_group;
 
 struct MacroGroup {
--- main-program/src/controller-buttons.cpp
+++ main-program/src/controller-buttons.cpp
@@ -8,63 +8,103 @@
 
 namespace controllerbuttons {
 
+class MacroHandler {
+  private:
+  pros::Mutex mutex;
+
+  void task_start_wrapper(void * void_ptr) {
+    ButtonStruct * struct_ptr = (ButtonStruct*) void_ptr;
+    for(auto group : struct_ptr->macro_groups) {
+      group->is_running_ptr = &struct_ptr->is_running;
+      group->group_task_t = &struct_ptr->button_task_t;
+    }
+
+    struct_ptr->is_running = true;
+    struct_ptr->function();
+    struct_ptr->is_running = false;
+    mutex.take(TIMEOUT_MAX);
+    mutex.give();
+    printf("ended\r\n");
+  }
+
+  public:
+  struct MacroGroup {
+    bool * is_running_ptr = &unnasigned_group;
+    pros::task_t * group_task_t;
+  };
+
+  void start(std::function<void()>) {
+
+  }
+
+  void interrupt_macro_group(MacroGroup * group) {
+    mutex.take(TIMEOUT_MAX);
+    printf("is_running_ptr = %d\r\n", *group->is_running_ptr);
+    if (*group->is_running_ptr) {
+      pros::c::task_delete(*group->group_task_t);
+      printf("DELETED!\r\n");
+      *group->is_running_ptr = false;
+    }
+    mutex.give();
+  }
+};
+
 
 class ButtonHandler {
-  private:
-  class ControllerCallback {
-    private:
-    class ButtonCallback {
-      private:
-      class ButtonTrigger {
+  public:
+  class Controller {
+    public:
+    class Button {
+      public:
+      class Trigger {
         private:
-        // bool run_as_task_;
         
-        // pros::task_t button_task_t_;
-
-        // void task_start() {
-        //   for(auto group : macro_groups) {
-        //     group->is_running_ptr = &is_running;
-        //     group->group_task_t = &button_task_t_;
-        //   }
-
-        //   is_running = true;
-        //   function_();
-        //   is_running = false;
-        //   // mutex.take(TIMEOUT_MAX);
-        //   // mutex.give();
-        //   printf("ended\r\n");
-        // }
-        
-        public:
         pros::Controller * controller_;
         pros::controller_digital_e_t button_;
 
         std::function<void()> function_;
+        std::vector<std::string> button_groups_;
+        bool run_as_task_;
+        
         bool was_triggered_ = false;
         // bool is_running;
-        bool is_set = false;
-        bool trigger_on_release;
+        bool is_set_ = false;
+        bool trigger_on_release_;
         // std::vector<MacroGroup *> macro_groups;
 
-        ButtonTrigger(pros::Controller * controller, pros::controller_digital_e_t button, bool trigger_on_release_arg) {
+        public:
+        Trigger(pros::Controller * controller,
+                pros::controller_digital_e_t button,
+                bool trigger_on_release) {
           controller_ = controller;
           button_ = button;
-          trigger_on_release = trigger_on_release_arg;
+          trigger_on_release_ = trigger_on_release;
         }
 
-        void set(std::function<void()> function) {
+        void set(std::function<void()> function,
+                 std::vector<std::string> button_groups = {"default"},
+                 bool run_as_task = false) {
           function_ = function;
-          is_set = true;
+          button_groups_ = button_groups;
+          run_as_task_ = run_as_task;
+          is_set_ = true;
         }
 
         void clear() {
-          is_set = false;
+          is_set_ = false;
+        }
+
+        void clear_group(std::string button_group) {
+          for (auto &group : button_groups_) {
+            if (group == button_group){
+              clear();
+              return;
+            }
+          }
         }
 
         void run_if_triggered() {
-          // printf("ButtonTrigger::run_if_triggered() is_set: %d\n", is_set);
-          if (is_set) {
-            // printf("ButtonTrigger::run_if_triggered() button: %d\n", button_);
+          if (is_set_) {
             bool is_pressing = controller_->get_digital(button_);
             bool was_pressed  = (is_pressing &&
                                 !was_triggered_);
@@ -79,10 +119,10 @@ class ButtonHandler {
             if (was_pressed && !is_running) {
               // Set the function to not run when the button is held
               was_triggered_ = true;
-              if (trigger_on_release) return;
+              if (trigger_on_release_) return;
             } else if (was_released) {
               was_triggered_ = false;
-              if (!trigger_on_release) return;
+              if (!trigger_on_release_) return;
             } else {
               return;
             }
@@ -98,100 +138,77 @@ class ButtonHandler {
         }
       };
 
-      
-      public:
+      Button(pros::Controller * controller,
+             pros::controller_digital_e_t button) :
+             pressed(controller, button, false),
+             released(controller, button, true) {}
 
-      pros::Controller * controller_;
-      pros::controller_digital_e_t button_;
-      
-      ButtonCallback(pros::Controller * controller, pros::controller_digital_e_t button) {
-        pressed = new ButtonTrigger(controller, button, false);
-        released = new ButtonTrigger(controller, button, true);
-      }
-
-      ButtonTrigger *pressed;
-      ButtonTrigger *released;
-
-      void run() {
-        // printf("ButtonCallback::run() button_: %d\n", button_);
-        // printf("ButtonCallback::run() pressed.button_: %d\n", pressed->button_);
-        pressed->run_if_triggered();
-        released->run_if_triggered();
-      }
+      Trigger pressed;
+      Trigger released;
+      std::vector<Controller::Button::Trigger *> triggers{&pressed, &released};
     };
+    
+    Controller(pros::Controller * controller) :
+               l1    (controller, pros::E_CONTROLLER_DIGITAL_L1),
+               l2    (controller, pros::E_CONTROLLER_DIGITAL_L2),
+               r1    (controller, pros::E_CONTROLLER_DIGITAL_R1),
+               r2    (controller, pros::E_CONTROLLER_DIGITAL_R2),
+               up    (controller, pros::E_CONTROLLER_DIGITAL_UP),
+               down  (controller, pros::E_CONTROLLER_DIGITAL_DOWN),
+               left  (controller, pros::E_CONTROLLER_DIGITAL_LEFT),
+               right (controller, pros::E_CONTROLLER_DIGITAL_RIGHT),
+               x     (controller, pros::E_CONTROLLER_DIGITAL_X),
+               b     (controller, pros::E_CONTROLLER_DIGITAL_B),
+               y     (controller, pros::E_CONTROLLER_DIGITAL_Y),
+               a     (controller, pros::E_CONTROLLER_DIGITAL_A) {}
 
-    pros::Controller * controller_;
-
-    public:
-    ControllerCallback(pros::Controller * controller) {
-      l1    = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_L1);
-      l2    = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_L2);
-      r1    = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_R1);
-      r2    = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_R2);
-      up    = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_UP);
-      down  = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_DOWN);
-      left  = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_LEFT);
-      right = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_RIGHT);
-      x     = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_X);
-      b     = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_B);
-      y     = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_Y);
-      a     = new ButtonCallback(controller, pros::E_CONTROLLER_DIGITAL_A);
-    }
-
-    ButtonCallback *l1;
-    ButtonCallback *l2;
-    ButtonCallback *r1;
-    ButtonCallback *r2;
-    ButtonCallback *up;
-    ButtonCallback *down;
-    ButtonCallback *left;
-    ButtonCallback *right;
-    ButtonCallback *x;
-    ButtonCallback *b;
-    ButtonCallback *y;
-    ButtonCallback *a;
-
-    std::vector<ButtonCallback *> button_vector = {l1, l2, r1, r2, up, down, left, right, x, b, y, a};
-
-    void run() {
-      for (auto &button_callback : button_vector) {
-        button_callback->run();
-      }
-    }
+    Button l1;
+    Button l2;
+    Button r1;
+    Button r2;
+    Button up;
+    Button down;
+    Button left;
+    Button right;
+    Button x;
+    Button b;
+    Button y;
+    Button a;
+    std::vector<Button *> buttons{&l1, &l2, &r1, &r2, &up, &down, &left, &right, &x, &b, &y, &a};
   };
 
-  pros::Controller * master_controller_;
-  pros::Controller * partner_controller_;
 
-  public:
-
-  // pros::Mutex mutex;
-
-  // void interrupt_macro_group(MacroGroup * group) {
-  //   mutex.take(TIMEOUT_MAX);
-  //   printf("is_running_ptr = %d\r\n", *group->is_running_ptr);
-  //   if (*group->is_running_ptr) {
-  //     pros::c::task_delete(*group->group_task_t);
-  //     printf("DELETED!\r\n");
-  //     *group->is_running_ptr = false;
-  //   }
-  //   mutex.give();
-  // }
-
-  ButtonHandler(pros::Controller * master_controller, pros::Controller * partner_controller) {
-    master = new ControllerCallback(master_controller);
-    partner = new ControllerCallback(partner_controller);
+  ButtonHandler(pros::Controller * master_controller,
+                pros::Controller * partner_controller) :
+                master(master_controller), 
+                partner(partner_controller) {
+    for (auto &controller : controllers) {
+      for (auto &button : controller->buttons) {
+        for (auto &trigger : button->triggers) { 
+          all_triggers.push_back(trigger);
+        }
+      }
+    }
   }
 
-  // ControllerCallback *master;
-  ControllerCallback *master;
-  ControllerCallback *partner;
-  
+  // Controller *master;
+  Controller master;
+  Controller partner;
+  std::vector<Controller *> controllers{&master, &partner};
+
+  std::vector<Controller::Button::Trigger *> all_triggers;
+
   void run() {
-    master->run();
-    partner->run();
+    for (auto &trigger : all_triggers) {
+      trigger->run_if_triggered();
+    }
   }
 
+  void clear_all() {
+    for (auto &trigger : all_triggers) {
+      trigger->clear();
+    }
+  }
 };
 
 ButtonHandler button_handler(&master, &partner);
@@ -206,15 +223,22 @@ void foo_two() {
 
 void foo_three() {
   printf("foo_three\n");
-  button_handler.master->a->pressed->set(&foo_two);
-  button_handler.master->a->released->set(&foo_one);
+  button_handler.clear_all();
+  set_callbacks();
+}
+
+void foo_four() {
+  printf("foo_four\n");
+  button_handler.master.a.pressed.set(foo_two, {"test"});
+  button_handler.master.a.released.set(foo_one, {"test"});
+  button_handler.master.x.pressed.set(foo_one, {"test"});
 }
 
 void set_callbacks() {
-  // MacroGroup test_group;
-  button_handler.master->a->pressed->set(&foo_one);
-  button_handler.master->a->released->set(&foo_two);
-  button_handler.master->b->released->set(&foo_three);
+  button_handler.master.a.pressed.set(foo_one, {"test"});
+  button_handler.master.a.released.set(foo_two, {"test"});
+  button_handler.master.b.pressed.set(foo_three, {"test"});
+  button_handler.master.y.pressed.set(foo_four, {"test"});
 }
 
 void run_buttons() {
