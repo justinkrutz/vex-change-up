8c85a48a20a5899bacc2df9bd301d2a976c03f4e
VEXObsessed <49462576+vexobsessed@users.noreply.github.com>
2020-08-13 11:29:45 -0500

    Added closestGoal

--- main-program/src/field-position.cpp
+++ main-program/src/field-position.cpp
@@ -1,6 +1,8 @@
-#include "api.h"
+// #include "api.h"
 
-#include "robot-config.h"
+// #include "robot-config.h"
+
+#include <bits/stdc++.h>
 
 enum BallColor {kRed, kBlue};
 
@@ -13,90 +15,37 @@ struct Ball {
 struct Goal {
   double x;
   double y;
-  std::vector<Ball> balls;
+  std::vector<BallColor> balls;
 };
 
 // std::vector<Ball> balls {
 //   {5.8, 5.8, {{kBlue}, {kRed}}},
 // };
 
-std::vector<Ball> balls_on_field {
-  {kBlue, 5.8, 5.8},
-  {kRed,  5.8, 5.8},
-
-  {kBlue, 70.3, 5.8},
-  {kRed,  70.3, 5.8},
-
-  {kBlue, 134.9, 5.8},
-  {kRed,  134.9, 5.8},
-
-  {kBlue, 12, 12},
-
+std::vector<Ball> match_balls_on_field {
+  {kBlue, 12,    12},
   {kBlue, 128.7, 12},
-
   {kRed,  70.3,  61.5},
-
-  {kBlue, 5.8, 70.3},
-  {kRed,  5.8, 70.3},
-  {kBlue, 5.8, 70.3},
-
-  {kBlue, 35, 70.3},
-  
-  {kBlue, 61.5, 70.3},
-
-  {kRed,  79.1, 70.3},
-
+  {kBlue, 35,    70.3},
+  {kBlue, 61.5,  70.3},
+  {kRed,  79.1,  70.3},
   {kRed,  105.7, 70.3},
-
-  {kRed,  134.9, 70.3},
-  {kBlue, 134.9, 70.3},
-  {kRed,  134.9, 70.3},
-  
-
-  {kRed,  5.8, 134.9},
-  {kBlue, 5.8, 134.9},
-
-  {kRed,  70.3, 134.9},
-  {kBlue, 70.3, 134.9},
-
-  {kRed,  134.9, 134.9},
-  {kBlue, 134.9, 134.9},
-
-  {kRed,  12, 12},
-
+  {kRed,  12,    12},
   {kRed,  128.7, 12},
-
-  {kBlue, 70.3, 79.1},
+  {kBlue, 70.3,  79.1},
 };
 
-Ball test_ball;
+std::vector<Ball> balls_on_field;
 
-std::vector<Goal> goals {
-  Goal{5.8, 5.8, {test_ball}}
-  // {{5.8, 5.8, kBlue},
-  // {5.8, 5.8, kRed}},
-  
-  // {{70.3, 5.8, kBlue},
-  // {70.3, 5.8, kRed}},
-  
-  // {{134.9, 5.8, kBlue},
-  // {134.9, 5.8, kRed}},
-  
-  // {{5.8, 70.3, kBlue},
-  // {5.8, 70.3, kRed},
-  // {5.8, 70.3, kBlue}},
-  
-  // {{134.9, 70.3, kRed},
-  // {134.9, 70.3, kBlue},
-  // {134.9, 70.3, kRed}}
-  
-
-  // {{5.8, 134.9, kRed},
-  // {5.8, 134.9, kBlue}},
-  
-  // {{70.3, 134.9, kRed},
-  // {70.3, 134.9, kBlue}},
-  
-  // {{134.9, 134.9, kRed},
-  // {134.9, 134.9, kBlue}}
+std::vector<Goal> match_goals {
+  {5.8, 5.8, {kBlue, kRed}},
+  {70.3, 5.8, {kBlue, kRed}},
+  {134.9, 5.8, {kBlue, kRed}},
+  {5.8, 70.3, {kBlue, kRed, kBlue}},
+  {134.9, 70.3, {kRed, kBlue, kRed}},
+  {5.8, 134.9, {kRed, kBlue}},
+  {70.3, 134.9, {kRed, kBlue}},
+  {134.9, 134.9, {kRed, kBlue}},
 };
+
+std::vector<Goal> goals;
