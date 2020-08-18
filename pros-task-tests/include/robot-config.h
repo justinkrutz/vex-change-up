0ae2a5b7517a6c7881b56a76da9427ee4998d7c9
VEXObsessed <49462576+vexobsessed@users.noreply.github.com>
2020-08-17 21:46:50 -0500

    Removed old projects

--- pros-task-tests/include/robot-config.h
+++
@@ -1,8 +0,0 @@
-#include "api.h"
-
-extern pros::Controller master;
-extern pros::Controller partner;
-extern pros::Motor test_motor_1;
-extern pros::Motor test_motor_2;
-extern pros::Vision vision_sensor;
-extern pros::Imu imu_sensor;
--- pros-task-tests/src/robot-config.cpp
+++
@@ -1,8 +0,0 @@
-#include "api.h"
-
-pros::Controller master (pros::E_CONTROLLER_MASTER);
-pros::Controller partner (pros::E_CONTROLLER_PARTNER);
-pros::Motor test_motor_1 (10, pros::E_MOTOR_GEARSET_18, false);
-pros::Motor test_motor_2 (20, pros::E_MOTOR_GEARSET_18, false);
-pros::Vision vision_sensor (7);
-pros::Imu imu_sensor (6);
