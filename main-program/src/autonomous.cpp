1bfb2d590fd2e42e595df75860c1bb35a5b14085
VEXObsessed <49462576+vexobsessed@users.noreply.github.com>
2020-08-17 21:50:20 -0500

    Revert "Moved main-program to root folder"
    This reverts commit 3e8b69dcc8491e6807f4b2743619b24949292df8.

---
+++ main-program/src/autonomous.cpp
@@ -0,0 +1,231 @@
+#include "main.h"
+#include "robot-functions.h"
+#include "autonomous.h"
+
+#include <bits/stdc++.h>
+#include "json.hpp"
+using json = nlohmann::ordered_json;
+
+std::string selected_auton_id = "0";
+
+enum BallColor {kOurs, kTheirs };
+
+struct Ball {
+  double x;
+  double y;
+  BallColor ball_color;
+};
+
+struct Goal {
+  double x;
+  double y;
+  std::vector<BallColor> balls;
+};
+
+std::vector<Ball> match_balls_on_field {
+  {12,    12,   kOurs},
+  {128.7, 12,   kOurs},
+  {70.3,  61.5, kTheirs},
+  {35,    70.3, kOurs},
+  {61.5,  70.3, kOurs},
+  {79.1,  70.3, kTheirs},
+  {105.7, 70.3, kTheirs},
+  {12,    12,   kTheirs},
+  {128.7, 12,   kTheirs},
+  {70.3,  79.1, kOurs},
+};
+
+std::vector<Ball> balls_on_field;
+
+/*     opposing alliance side
+   │             │
+   └─────────────┘
+┌───────────────────┐
+│7        8        9│
+│─────────╩─────────│
+│                   │
+│4════════5════════6│
+│                   │
+│─────────╦─────────│
+│1        2        3│
+└───────────────────┘
+   ┌─────────────┐
+   │             │
+      alliance     */
+
+
+std::vector<Goal> match_goals {
+  {5.8, 5.8, {kOurs, kTheirs}},
+  {70.3, 5.8, {kOurs, kTheirs}},
+  {134.9, 5.8, {kOurs, kTheirs}},
+  {5.8, 70.3, {kOurs, kTheirs, kOurs}},
+  {70.3, 70.3, {}},
+  {134.9, 70.3, {kTheirs, kOurs, kTheirs}},
+  {5.8, 134.9, {kTheirs, kOurs}},
+  {70.3, 134.9, {kTheirs, kOurs}},
+  {134.9, 134.9, {kTheirs, kOurs}},
+};
+
+std::vector<Goal> skills_goals {
+  {5.8, 5.8, {kTheirs, kTheirs}},
+  {70.3, 5.8, {kTheirs}},
+  {134.9, 5.8, {kTheirs, kTheirs}},
+  {5.8, 70.3, {kTheirs}},
+  {70.3, 70.3, {kTheirs, kTheirs, kTheirs}},
+  {134.9, 70.3, {kTheirs}},
+  {5.8, 134.9, {kTheirs, kTheirs}},
+  {70.3, 134.9, {kTheirs}},
+  {134.9, 134.9, {kTheirs, kTheirs}},
+};
+
+std::vector<Goal> goals;
+
+double proximity (double x_one, double y_one, double x_two, double y_two) {
+  return fabs(sqrt(pow(x_one - x_two, 2) + pow(y_one - y_two, 2)));
+}
+
+template <typename T>
+T closestObject(double x, double y, std::vector<T> objects) {
+  T closest_object;
+  for (auto &object : objects) {
+    if (proximity(x, y, object.x, object.y) <
+        proximity(x, y, closest_object.x, closest_object.y)) {
+      closest_object = object;
+    }
+  }
+  return closest_object;
+}
+
+void driveToClosestGoal() {
+  QLength x_pos = chassis->getState().x;
+  QLength y_pos = chassis->getState().y;
+  Goal goal = closestObject<Goal>(x_pos.convert(inch), y_pos.convert(inch), match_goals);
+  chassis->driveToPoint({goal.x * inch, goal.y * inch}, false, 14_in);
+  // chassis->driveToPoint({10 * inch, 10 * inch});
+  // chassis->driveToPoint({70.3 * inch, 134.9 * inch}, false, 14_in);
+}
+
+
+// json all_autons;
+
+// void loadAllAutons() {
+//   std::ifstream i("/usd/autonomous_routines.json");
+//   i >> all_autons;
+//   i.close();
+// }
+
+std::string getNewAutonId(json autons) {
+  int largest_id_int = 0;
+  for (auto & id_json : autons.items()) {
+    int id_int = std::stoi(id_json.key(), nullptr);
+    if (id_int > largest_id_int) {
+      largest_id_int = id_int;
+    }
+  }
+  return std::to_string(largest_id_int + 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+json AutonManager::all_autons = {};
+
+void AutonManager::loadAutonsFromSD() {
+  std::ifstream i("/usd/autonomous_routines.json");
+  i >> all_autons;
+  i.close();
+}
+
+void AutonManager::saveAutonsToSD() {
+  std::ofstream o("/usd/autonomous_routines.json");
+  o << std::setw(2) << all_autons << std::endl;
+  o.close();
+}
+
+AutonManager::AutonManager(std::string auton_id)
+              : auton_id(auton_id), auton_steps(all_autons[auton_id]["steps"]){}
+
+void AutonManager::run() {
+  json last_waypoint = {};
+  for (auto& step : auton_steps.items()) {
+    if (step.value()["stepType"] == "waypoint") {
+      // double test = step.value()["x"];
+      QLength x = step.value()["x"] * inch;
+      QLength y = step.value()["y"] * inch;
+      QAngle theta = step.value()["theta"] * degree;
+      robotfunctions::driveToPosition(x, y, theta);
+      last_waypoint = step.value();
+    // }
+    } else if (step.value()["stepType"] == "driveToBallAndIntake") {
+      QLength x = last_waypoint["x"] * inch;
+      QLength y = last_waypoint["y"] * inch;
+      QAngle theta = last_waypoint["theta"] * degree;
+      robotfunctions::driveToPosition(x, y, theta);
+      robotfunctions::intakeBalls(step.value()["ballsIn"]);
+      robotfunctions::intakeBalls(step.value()["ballsOut"]);
+    } else if (step.value()["stepType"] == "driveToGoalAndCycle") {
+      driveToClosestGoal();
+    }
+  }
+}
+
+void AutonManager::save() {
+  all_autons[auton_id]["name"] = "test auton";
+  all_autons[auton_id]["autonType"] = "match";
+  all_autons[auton_id]["steps"] = auton_steps;
+}
+
+void AutonManager::nextStep() {
+  selected_step = std::min(selected_step + 1, int(auton_steps.size() - 1));
+}
+
+void AutonManager::previousStep() {
+  selected_step = std::max(selected_step - 1, 0);
+}
+
+void AutonManager::setStepDriveToGoalAndScore(int balls_in, int balls_out) {
+  json step;
+  step["stepType"] = "driveToGoalAndCycle";
+  step["ballsIn"] = balls_in;
+  step["ballsOut"] = balls_out;
+  auton_steps[selected_step] = step;
+}
+
+void AutonManager::insertStep() {
+  json step = {};
+  auton_steps.insert(auton_steps.begin() + ++selected_step, step);
+}
+
+void AutonManager::removeStep() {
+  auton_steps.erase(selected_step);
+}
+
+void AutonManager::setStepWaypoint() {
+  auton_steps[selected_step]["stepType"] = "waypoint";
+  QLength x = chassis->getState().x;
+  QLength y = chassis->getState().y;
+  QAngle theta = chassis->getState().theta;
+  auton_steps[selected_step]["x"] = x.convert(inch);
+  auton_steps[selected_step]["y"] = y.convert(inch);
+  auton_steps[selected_step]["theta"] = theta.convert(degree);
+}
+
+// void jsonTest() {
+//   loadAllAutons();
+//   std::cout << "all_autons before: " << all_autons.dump(2) << "\n";
+//   json new_auton = all_autons["0"];
+//   all_autons[getNewAutonId(all_autons)] = new_auton;
+//   std::ofstream o("/usd/autonomous_routines.json");
+//   o << std::setw(2) << all_autons << std::endl;
+//   o.close();
+//   std::cout << "all_autons after: " << all_autons.dump(2) << "\n";
+// }
---
+++ main-program/src/controller-menu.cpp
@@ -0,0 +1,360 @@
+#include "api.h"
+
+#include <bits/stdc++.h>
+#include "json.hpp"
+using json = nlohmann::ordered_json;
+
+#include "robot-config.h"
+#include "controller-buttons.h"
+#include "controller-menu.h"
+#include "robot-functions.h"
+#include "autonomous.h"
+
+using namespace controllerbuttons;
+
+namespace controllermenu {
+
+std::string controller_print_array [3];
+
+void controller_print() {
+  std::string array_last [3];
+  while (true) {
+    for (int i = 0; i < 3; i++) {
+      if (controller_print_array[i] != array_last[i]) {
+        array_last[i] = controller_print_array[i];
+        char print_str[20];
+        controller_print_array[i].resize(19);
+        sprintf(print_str, "%-19s", controller_print_array[i].c_str());
+        master.print(i, 0, print_str);
+        pros::delay(50);
+      }
+    }
+    pros::delay(10);
+  }
+}
+
+class MenuItem;
+MenuItem *current_item;
+
+class MenuFolder;
+MenuFolder *root_folder;
+
+
+enum MenuItemType {
+  kFolder,
+  kAutonomous,
+  kAction,
+};
+
+std::map<MenuItemType, const char*> MenuItemTypeName = {
+  {kFolder,           "Folder"},
+  {kAutonomous,       "Autonomous"},
+  {kAction, "Action"},
+};
+
+
+class MenuItem {
+  public:
+  MenuItem(MenuItemType item_type, std::string name, std::vector<MenuItem*> children = {})
+           : item_type(item_type), name(name), children(children) {}
+
+  MenuItemType item_type;
+  std::string name;
+  std::vector<MenuItem*> children;
+
+  virtual void print() {
+    printf("virtual void print()\n");
+  }
+
+  virtual void set_callbacks() {
+    button_handler.clear_group("menu");
+    button_handler.master.b.pressed.set ([this](){ back(); }, {"menu"});
+  }
+
+  MenuItem * findParent(MenuItem *current_folder = (MenuItem *) root_folder);
+
+  void back();
+};
+
+class MenuScroll {
+  public:
+  MenuScroll(MenuItem *menu_item, int number_of_items) : menu_item(menu_item), number_of_items(number_of_items) {}
+
+  int cursor_location = 0;
+
+  int number_of_items;
+
+  void print() {
+    std::string selection = "[_][_][_][_][_]";
+    if (number_of_items < 6) {
+      selection.resize(number_of_items * 3);
+      selection.replace(cursor_location * 3 + 1, 1, "o");
+    } else if (cursor_location > 2 && cursor_location < number_of_items - 3) {
+      selection.replace(1, 1, "<");
+      selection.replace(13, 1, ">");
+      selection.replace(7, 1, "o");
+    } else if (cursor_location > 2) {
+      selection.replace(1, 1, "<");
+      selection.replace((cursor_location - number_of_items + 5) * 3 + 1, 1, "o");
+    } else if (cursor_location < number_of_items - 3) {
+      selection.replace(13, 1, ">");
+      selection.replace(cursor_location * 3 + 1, 1, "o");
+    }
+    controller_print_array[0] = selection;
+  }
+
+  void set_callbacks() {
+    button_handler.clear_group("menu");
+    button_handler.master.right.pressed.set ([this](){ scroll(1); }, {"menu"});
+    button_handler.master.left.pressed.set  ([this](){ scroll(-1); },  {"menu"});
+  }
+
+  private:
+  MenuItem *menu_item;
+
+  void scroll(int scroll_amount) {
+    if (scroll_amount + cursor_location >= 0 && scroll_amount + cursor_location < number_of_items) {
+      cursor_location += scroll_amount;
+      menu_item->print();
+    }
+  }
+};
+
+class MenuFolder : public MenuItem {
+  public:
+  MenuFolder(std::string name, std::vector<MenuItem*> children, MenuItemType item_type = kFolder)
+         : MenuItem(item_type, name, children), folderScroll(this, children.size()) {}
+
+  void set_callbacks() {
+    button_handler.clear_group("menu");
+    folderScroll.set_callbacks();
+    button_handler.master.a.pressed.set ([this](){ select(); }, {"menu"});
+    button_handler.master.b.pressed.set ([this](){ back(); },   {"menu"});
+  }
+
+  void print() {
+    if (folderScroll.number_of_items > 0) {
+      folderScroll.print();
+      controller_print_array[1] = MenuItemTypeName.at(children[folderScroll.cursor_location]->item_type);
+      controller_print_array[2] = children[folderScroll.cursor_location]->name;
+    } else {
+      controller_print_array[0] = "Folder is empty";
+      controller_print_array[1] = "";
+      controller_print_array[2] = "";
+    }
+  }
+
+  void select() {
+    if (children.size() > 0) {
+      current_item = children[folderScroll.cursor_location];
+      current_item->set_callbacks();
+      current_item->print();
+    }
+  }
+
+  private:
+  MenuScroll folderScroll;
+};
+
+MenuItem * MenuItem::findParent(MenuItem *current_folder) {
+  if (this == root_folder) {
+    return root_folder;
+  }
+  for (MenuItem *child : current_folder->children) {
+    if (this == child) {
+      return current_folder;
+    }
+    MenuItem* sub_child = findParent((MenuItem*)child);
+    if (sub_child) {
+      return sub_child;
+    }
+  }
+  return NULL;
+}
+
+void MenuItem::back() {
+  current_item = findParent();
+  current_item->set_callbacks();
+  current_item->print();
+}
+
+class MenuAutonomous;
+
+class MenuAction : public MenuItem {
+  public:
+  MenuAction(MenuAutonomous *menu_autonomous,
+             std::string name,
+             std::string message_one,
+             std::string message_two,
+             std::function<void()> function)
+           : MenuItem(kAction, name),
+             message_one_(message_one),
+             message_two_(message_two),
+             menu_autonomous_(menu_autonomous),
+             function_(function) {}
+
+  void print();
+  private:
+  MenuAutonomous *menu_autonomous_;
+  std::string message_one_;
+  std::string message_two_;
+  std::function<void()> function_;
+};
+
+class MenuCreateAuton : public MenuItem {
+  public:
+  MenuCreateAuton(std::string name) : MenuItem(kAction, name), auton("1") {}
+
+  void print() {
+    chassis->setState({0_in, 0_in, 0_deg});
+    pros::Task([this](){
+      while (current_item == this) {
+        double x = chassis->getState().x.convert(inch);
+        double y = chassis->getState().y.convert(inch);
+        double theta = chassis->getState().theta.convert(degree);
+        std::string x_str     = std::to_string((int)x);
+        std::string y_str     = std::to_string((int)y);
+        std::string theta_str = std::to_string((int)theta);
+        controller_print_array[0] = "x: " + x_str + " y: " + y_str + " t: " + theta_str;
+        controller_print_array[1] = "step " + std::to_string(auton.selected_step + 1) + " of " + std::to_string(auton.auton_steps.size());
+        // controller_print_array[2] = "x: " + std::to_string((int)auton.auton_steps["x"])
+        //     + " y: " + std::to_string((int)auton.auton_steps["y"])
+        //     + " t: " + std::to_string((int)auton.auton_steps["theta"]);
+        pros::delay(150);
+      }
+    });
+  }
+
+  void set_callbacks() {
+    button_handler.clear_group("menu");
+    button_handler.master.b.pressed.set ([this](){ back(); }, {"menu"});
+    button_handler.master.a.pressed.set ([this](){
+      auton.save();
+      AutonManager::saveAutonsToSD();
+    }, {"menu"});
+    button_handler.master.y.pressed.set ([this](){ auton.run(); }, {"menu"});
+    button_handler.master.x.pressed.set ([this](){ auton.setStepWaypoint(); }, {"menu"});
+    button_handler.master.up.pressed.set ([this](){ auton.insertStep(); }, {"menu"});
+    button_handler.master.down.pressed.set ([this](){ auton.removeStep(); }, {"menu"});
+    button_handler.master.left.pressed.set ([this](){ auton.previousStep(); }, {"menu"});
+    button_handler.master.right.pressed.set ([this](){ auton.nextStep(); }, {"menu"});
+    button_handler.master.r1.pressed.set ([this](){ /*drive to nearest goal*/ }, {"menu"});
+    button_handler.master.r2.pressed.set ([this](){ /*drive to and intake nearest ball*/ }, {"menu"});
+    button_handler.master.l1.pressed.set ([this](){ /*score one ball if at goal*/ }, {"menu"});
+    button_handler.master.l2.pressed.set ([this](){ /*intake one ball if at goal*/ }, {"menu"});
+  }
+
+  private:
+  AutonManager auton;
+  // std::string auton_id = getNewAutonId(AutonManager::all_autons);
+};
+
+// class MenuAutonomousSelect : public MenuItem {
+//   MenuAutonomousSelect(MenuAutonomous *menu_autonomous) : MenuItem(kAutonomousSelect, "Select for match"), menu_autonomous(menu_autonomous) {}
+
+//   void print();
+//   private:
+//   MenuAutonomous *menu_autonomous;
+// };
+
+// class MenuAutonomousRun : public MenuItem {
+//   public:
+//   MenuAutonomousRun(MenuAutonomous *menu_autonomous) : MenuItem(kAutonomousRun, "Run immediately"), menu_autonomous(menu_autonomous) {}
+
+//   void print();
+//   private:
+//   MenuAutonomous *menu_autonomous;
+// };
+
+// class MenuAutonomousEdit : public MenuItem {
+//   public:
+//   MenuAutonomousEdit(MenuAutonomous *menu_autonomous) : MenuItem(kAutonomousEdit, "Changes saved to SD"), menu_autonomous(menu_autonomous) {}
+
+//   void print();
+//   private:
+//   MenuAutonomous *menu_autonomous;
+// };
+
+
+
+class MenuAutonomous : public MenuFolder {
+  public:
+  MenuAutonomous(std::string name, std::string json_key) 
+      : MenuFolder(name, {
+          new MenuAction(this, "Select", "Selected:", "Waiting for enable", [this](){ select(); }),
+          new MenuAction(this, "Run", "Running","Press 'B' to abort", [this](){ run(); }),
+          new MenuAction(this, "Edit", "Editing", "Press 'A' to save", [this](){ edit(); })
+        },
+        kAutonomous),
+        json_key(json_key) {}
+
+  void select() {}
+  void run() {}
+  void edit() {}
+
+  private:
+  std::string json_key;
+};
+
+void MenuAction::print() {
+  controller_print_array[0] = message_one_;
+  controller_print_array[1] = menu_autonomous_->name;
+  controller_print_array[2] = message_two_;
+  function_();
+}
+
+
+
+
+
+
+
+std::vector<MenuItem *> getMenuAutonsFromJson(json autons, std::string auton_type) {
+  std::vector<MenuItem *> auton_items;
+  for (auto & auton : autons.items()) {
+    // printf("autonType: %s\n", auton.value()["autonType"]);
+    if (auton.value()["autonType"] == auton_type) {
+      std::string auton_name;
+      if (auton.value()["name"] == "") {
+        auton_name = "Untitled-" + auton.key();
+      } else {
+        auton_name = auton.value()["name"];
+      }
+      auton_items.push_back(new MenuAutonomous(auton_name, auton.key()));
+    }
+  }
+  return auton_items;
+}
+
+void createFolderStructure() {
+  root_folder = new MenuFolder("", {
+  new MenuFolder("Match Autons", {
+    getMenuAutonsFromJson(AutonManager::all_autons, "match")
+  }),
+  new MenuFolder("Skills Autons", {
+    getMenuAutonsFromJson(AutonManager::all_autons, "skills")
+  }),
+  new MenuFolder("Auton Builders", {
+    new MenuCreateAuton("Build Match"),
+    // new Autonomous("Build Skills"),
+  }),
+  // new MenuFolder("Other", {
+  // }),
+  // new MenuFolder("Actions", {
+  // }),
+  // new MenuFolder("Settings", {
+  // })
+});
+}
+
+void init() {
+  createFolderStructure();
+  master.clear();
+  pros::delay(50);
+  current_item = root_folder;
+  pros::Task controller_print_task (controller_print);
+  current_item->set_callbacks();
+  current_item->print();
+}
+
+} // namespace controllermenu
